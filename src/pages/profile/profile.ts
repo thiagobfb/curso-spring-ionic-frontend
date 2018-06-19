c7564d78d345807f493bae9569f4124b7dcbd5a1
Thiago Bernardo <thiagobf.barbosa@gmail.com>
2018-06-18 23:25:00 -0300

    Bugfix - atualizacao da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -16,13 +17,17 @@ export class ProfilePage {
   cliente: ClienteDTO;
   picture: string;
   cameraOn: boolean;
+  profileImage;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -52,8 +57,24 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
       .subscribe(response => {
         this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        this.blobToDataUrl(response).then(dataUrl => {
+          let str: string = dataUrl as string;
+          this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+        });
       },
-        error => { });
+      error => {
+        this.profileImage = 'assets/imgs/avatar-blank.png';
+      });
+  }
+
+  //https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataUrl(blob) {
+    return new Promise((fullfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fullfill(reader.result);
+      reader.readAsDataURL(blob);
+    });
   }
 
   getCameraPicture() {
@@ -99,7 +120,7 @@ export class ProfilePage {
     this.clienteService.uploadImage(this.picture)
       .subscribe(response => {
         this.picture = null;
-        this.loadData();
+        this.getImageIfExists();
       },
       error => { });
   }
